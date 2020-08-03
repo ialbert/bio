08f41340207229c8324ac97629936b30189df56e
Istvan Albert <istvan.albert@gmail.com>
2020-08-03 16:45:24 -0400

    code update by ialbert

--- biorun/__init__.py
+++ biorun/__init__.py
@@ -1,2 +1,12 @@
-
 VERSION = "0.0.1"
+
+from jinja2 import Template
+
+
+def render(text, ctx={}):
+    """
+    Renders a template with a context.
+    """
+    tmpl = Template(text, trim_blocks=True, lstrip_blocks=True, autoescape=False)
+    text = tmpl.render(ctx)
+    return text
