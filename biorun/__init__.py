7273e42a240e8fd84c5a8e2676eb201adc86cdfe
Natay Aberra <natay.aberra@gmail.com>
2020-09-11 10:37:57 -0400

    clean up

--- biorun/__init__.py
+++ biorun/__init__.py
@@ -3,9 +3,7 @@ import os
 
 
 __CURRENT_DIR = os.path.dirname(__file__)
-__DUMP = os.path.join(__CURRENT_DIR, '..', 'save_file')
+DUMP_DIR = os.path.join(__CURRENT_DIR, '..', 'dump')
 
 VERSION = "0.0.1"
 
-# Default directory used to store data
-DUMP_DIR = os.path.abspath(os.getenv('DUMP_DIR', __DUMP))
