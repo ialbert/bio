a6156d161ced2f9d9a3c467ac4bab2b851a020bd
Natay Aberra <natay.aberra@gmail.com>
2020-09-11 08:59:15 -0400

    update

--- biorun/convert/parse.py
+++ biorun/convert/parse.py
@@ -28,43 +28,43 @@ def fasta_converter(in_file, in_format, out_file, verb=0):
     input_handle = open(in_file, 'r')
     stream = SeqIO.parse(input_handle, in_format)
 
-    # Format the fasta file
+    # Make sure the file is in fasta format.
     formatter = lambda seq_record: f">{seq_record.id} {seq_record.description}\n{seq_record.seq}\n"
 
     # Download fasta file into out file
-    utils.download(stream, outname=out_file, buffer=1024, verb=verb, formatter=formatter)
+    utils.download(stream, outname=out_file, verb=verb, formatter=formatter)
 
     return out_file
 
 
 @plac.pos('acc', "accession number")
-@plac.opt('db', "target database ")
-@plac.opt('mode', "output mode")
+@plac.opt('db', "input target database ")
+@plac.opt('mode', "input mode")
 @plac.opt('output_dir', "output directory")
-@plac.opt('frmat', "output data format")
+@plac.opt('format', "output data format")
 @plac.flg('update', "overwrite existing data")
 @plac.opt('verbosity', "verbosity level")
-def run(acc, db='nuccore', in_format='gb', mode='text', output_dir=None, frmat=FASTA, update=False,
+def run(acc, db='nuccore', in_format='gb', mode='text', output_dir=None, format=FASTA, update=False,
         verbosity=0):
 
-    # Get a local copy of the file or download it using Entrez.
+    # Get a local copy of the genbank file or download it using Entrez.
     in_file = fetch.save_or_get(acc=acc, db=db, format=in_format,
                                 mode=mode,
                                 output_dir=output_dir,
                                 update=update,
                                 verbosity=verbosity)
 
-    # Pass file for further processing.
-    outname = utils.resolve_fname(acc=acc, directory=output_dir, ext=frmat)
+    # Resolve the converted output name
+    outname = utils.resolve_fname(acc=acc, directory=output_dir, ext=format)
 
+    # Do not override existing file without update flag.
     if not update and os.path.isfile(outname):
+        msg = f"File already exists at: {outname}"
+        utils.print_message(msg=msg, styles=[utils.BOLD], verb=verbosity)
         return
 
-    # converted = converter(in_file=in_file, in_format=in_format, out_file=outname, out_format=frmat)
+    # Convert input file and write into outname.
+    #converted = converter(in_file=in_file, in_format=in_format, out_file=outname, out_format=format)
     converted = fasta_converter(in_file=in_file, in_format=in_format, out_file=outname, verb=0)
-    
-    # if frmat == FASTA:
-    #     converted = fasta_converter(in_file=in_file, in_format=in_format, out_file=outname, verb=0)
-    #     pass
 
     return
