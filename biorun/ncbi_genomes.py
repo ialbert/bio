9d10d75a93d1ce6b106d993c65ebee17d0d57868
Natay Aberra <natay.aberra@gmail.com>
2021-01-08 14:53:14 -0500

    update in genome

--- biorun/ncbi_genomes.py
+++ biorun/ncbi_genomes.py
@@ -10,22 +10,9 @@ join = os.path.join
 
 ASSEMBLY_URL = "https://ftp.ncbi.nih.gov/genomes/ASSEMBLY_REPORTS/assembly_summary_genbank.txt"
 ASSEMBLY_FILE_NAME = "assembly_summary_genbank.txt"
-SQLITE_DB = "genome.db"
-JSON_DB = "genome.json"
 
 # Create the full paths
 ASSEMBLY_FILE_NAME = join(utils.DATADIR, ASSEMBLY_FILE_NAME)
-
-SQLITE_DB = join(utils.DATADIR, SQLITE_DB)
-JSON_DB = join(utils.DATADIR, JSON_DB)
-
-# Table names
-ACC = "ACCESSIONS"
-
-GCF = "GCF"
-
-# Relate accession to asm_name
-ASM = 'ASM'
 
 
 def resolve_fname(name, suffix='genomic.gbff.gz'):
@@ -35,28 +22,30 @@ def resolve_fname(name, suffix='genomic.gbff.gz'):
     return fname
 
 
-def get_data(preload=False):
+def fetch_data(url, acc, update=False):
 
-    if preload:
-        if not os.path.isfile(JSON_DB):
-            utils.error(f"File not found (you must build it first): {JSON_DB}")
-        store = json.load(open(JSON_DB))
-        accs = store[ACC]
-        gca2gcf = store[GCF]
-        asm = store[ASM]
+    suffix = 'genomic.gbff.gz'
 
+    end = f"{os.path.basename(url)}_{suffix}"
+    url = f"{url}/{end}"
+    dest = resolve_fname(acc, suffix=suffix)
+
+    # File already exists
+    if os.path.exists(dest) and not update:
+        print(f"*** {acc} found at {dest}")
     else:
-        accs = utils.open_db(ACC, fname=SQLITE_DB)
-        gca2gcf = utils.open_db(GCF, fname=SQLITE_DB)
-        asm = utils.open_db(ASM, fname=SQLITE_DB)
+        utils.download(url=url, dest_name=dest)
 
-    return accs, gca2gcf, asm
+    return
 
 
-def parse_file(fname=ASSEMBLY_FILE_NAME):
+def search_file(fname=ASSEMBLY_FILE_NAME, accs=None, update=False):
     """
-    Parse the file
+    Parse and search file
     """
+
+    accs = [] if accs is None else accs
+
     if not os.path.isfile(fname):
         utils.error("assembly information not found (download first)")
 
@@ -65,10 +54,18 @@ def parse_file(fname=ASSEMBLY_FILE_NAME):
     stream = filter(lambda x: x[0] != '#', stream)
     stream = csv.reader(stream, delimiter='\t')
 
-    accs = dict()
-    # Dict keyed by tax ids and point to a list of accession numbers.
-    gca2gcf = dict()
-    asm_names = dict()
+    visited = set()
+    accs = set(accs)
+    nhits = 0
+
+    def hit(a, r, g):
+        hits = []
+        # Iterate and collect hits
+        for ac in accs:
+            val = ac.strip()
+            if (val == a or val == r or val == g) and ac not in visited:
+                hits.append(val)
+        return hits
 
     for line in stream:
 
@@ -76,46 +73,28 @@ def parse_file(fname=ASSEMBLY_FILE_NAME):
         row = dict(zip(const.GENOMES_HEADER, line))
 
         acc = row['assembly_accession']
-        tid = row['taxid']
+        root = acc.split('.')[0]
         gcf = row['gbrs_paired_asm']
-        asm = row['asm_name']
         path = row['ftp_path']
 
-        accs[acc] = dict(tid=tid, path=path, gcf=gcf, asm=asm)
+        # Check to see if this assembly is in the query list
+        matches = hit(acc, root, gcf)
 
-        gca2gcf[gcf] = acc
-        asm_names[asm] = acc
+        # Fetch the data if found
+        if matches:
+            fetch_data(url=path, acc=acc, update=update)
+            visited.update([acc, root, gcf])
+            nhits += 1
 
-    return accs, gca2gcf, asm_names
+        # Stop reading through file once all queries have been found.
+        if nhits == len(accs):
+            break
 
+    # Notify if some are not found
+    missing = accs.difference(visited)
 
-def build_database(fname=ASSEMBLY_FILE_NAME):
-
-    # Check the source file exists
-    if not os.path.isfile(fname):
-        utils.error("assembly information not found (download first)")
-
-    # Parse accession numbers and
-    accs_dict, gca2gcf, asm_names = parse_file(fname=fname)
-
-    def save(table, vals):
-        utils.save_table(table, vals, fname=SQLITE_DB)
-
-    save(ACC, accs_dict)
-
-    save(GCF, gca2gcf)
-
-    save(ASM, asm_names)
-
-    print("*** saving the JSON model")
-
-    # JSON will only have the graph and names.
-    store = dict(ACC=accs_dict, GCF=gca2gcf, ASM=asm_names)
-    fp = open(JSON_DB, 'wt')
-    json.dump(store, fp, indent=4)
-    fp.close()
-
-    return accs_dict, gca2gcf, asm_names
+    if missing:
+        print(f'*** {",".join(missing)} not found, skipped ...')
 
 
 def update_assembly(url=ASSEMBLY_URL, dest_name=ASSEMBLY_FILE_NAME):
@@ -125,46 +104,12 @@ def update_assembly(url=ASSEMBLY_URL, dest_name=ASSEMBLY_FILE_NAME):
     utils.download(url=url, dest_name=dest_name)
 
 
-def fetch_data(accs, adict, gcfdict, asm):
-    """
-    Fetch accession data
-    """
-
-    # Build first if the database does not exist.
-    suffix = 'genomic.gbff.gz'
-
-    for acc in accs:
-        # Search for this number in the GCF_, asm and accession dict
-        gca = gcfdict.get(acc)
-        asm_name = asm.get(acc)
-        data = adict.get(acc) or adict.get(gca) or adict.get(asm_name)
-
-        if not data:
-            print(f"*** {acc} not found, skipping ...")
-            continue
-
-        url = data['path']
-        end = f"{os.path.basename(url)}_{suffix}"
-        url = f"{url}/{end}"
-
-        # Where tp store the file
-        dest = resolve_fname(acc, suffix=suffix)
-
-        #if os.path.exists(dest):
-        # Get the destination files
-        # Download file into
-        utils.download(url=url, dest_name=dest)
-
-    return
-
-
 @plac.pos("accs", "data names")
-@plac.flg('update', "obtain the latest taxdump from NCBI")
-@plac.flg('build', "Build the database")
+@plac.flg('update', "Update the genome assembly file")
 @plac.flg('genome', "Execute", abbrev='x')
 @plac.flg('download', "downloads the database from the remote site", abbrev='d')
 @plac.flg('verbose', "verbose mode, prints more messages")
-def run(update=False, download=False, build=False, genome=False, verbose=False, *accs):
+def run(update=False, download=False, genome=False, verbose=False, *accs):
     """
     Downloads genome assembly data from NCBI.
     """
@@ -172,14 +117,8 @@ def run(update=False, download=False, build=False, genome=False, verbose=False, 
         update_assembly()
         return
 
-    if build:
-        # If the database does not exist, then
-        build_database()
-        return
-
-    adict, gcf2gca, asm = get_data()
-
-    fetch_data(accs=accs, adict=adict, gcfdict=gcf2gca, asm=asm)
+    # Search for accession numbers and download assembly if any found.
+    search_file(accs=accs, update=update)
 
 
 #
