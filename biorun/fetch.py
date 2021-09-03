eec667e023bf9e1e103a988fc217090b72b173bc
Istvan Albert <istvan.albert@gmail.com>
2021-09-03 13:16:45 -0400

    docs updated by ialbert

--- biorun/fetch.py
+++ biorun/fetch.py
@@ -16,59 +16,49 @@ from urllib.error import HTTPError
 
 Entrez.email = 'foo@foo.com'
 
-ncbi_patt = r'(?P<letters>[a-zA-Z]+)(?P<under>_?)(?P<digits>\d+)(\.(?P<version>\d+))?'
-ncbi_patt = re.compile(ncbi_patt)
+NCBI_PATT = r'(?P<letters>[a-zA-Z]+)(?P<under>_?)(?P<digits>\d+)(\.(?P<version>\d+))?'
+NCBI_PATT = re.compile(NCBI_PATT)
 
 
 #
-# Refseq accession numbers
+# Genbank and Refseq accession numbers
 #
+# https://www.ncbi.nlm.nih.gov/genbank/acc_prefix/
 # https://www.ncbi.nlm.nih.gov/books/NBK21091/table/ch18.T.refseq_accession_numbers_and_mole/?report=objectonly/
 #
 
-class Accession:
+def parse_ncbi(text):
+    m = NCBI_PATT.search(text)
+    code = m.group("letters") if m else ''
+    digits = m.group("digits") if m else ''
+    refseq = m.group("under") if m else ''
+    version = m.group("version") if m else ''
+    return code, digits, refseq
+
+def is_ncbi_nucleotide(text):
     """
-    Represents an accession number
+    Returns true of text matches NCBI nucleotides.
     """
+    code, digits, refseq = parse_ncbi(text)
+    if refseq:
+        cond = code in ["AC", "NC", "NG", "NT", "NW", "NZ", "NM", "XM", "XR"]
+    else:
+        num1, num2 = len(code), len(digits)
+        cond = (num1 == 1 and num2 == 5) or (num1 == 2 and num2 == 6) or (num1 == 3 and num2 == 8)
 
-    def __init__(self, value):
-        self.value = value
+    return cond
 
-        try:
-            int(self.value)
-            self.is_int = True
-        except ValueError as exc:
-            self.is_int = False
-
-        m = ncbi_patt.search(value)
-
-        # Valid accession numbers
-        self.valid = bool(m) or self.is_int
-
-        self.code = m.group("letters") if m else ''
-        self.digits = m.group("digits") if m else ''
-        self.under = m.group("under") if m else ''
-        self.version = m.group("version") if m else ''
-
-        # It is refseg if it has underscore
-        self.refseq = bool(self.under)
-
-    def is_nucleotide(self):
-        if self.code:
-            cond = self.code in ["AC", "NC", "NG", "NT", "NW", "NZ", "NM", "XM", "XR"]
-        else:
-            num1, num2 = len(self.code), len(self.digits)
-            cond = (num1 == 1 and num2 == 5) or (num1 == 2 and num2 == 6) or (num1 == 3 and num2 == 8)
-
-        return cond
-
-    def is_protein(self):
-        if self.refseq:
-            cond = self.code in ["AP", "NP", "YP", "XP", "WP"]
-        else:
-            num1, num2 = len(self.code), len(self.digits)
-            cond = (num1 == 3 and num2 == 5) or (num1 == 3 and num2 == 7)
-        return cond
+def is_ncbi_protein(text):
+    """
+    Returns true of text matches NCBI protein sequences
+    """
+    code, digits, refseq = parse_ncbi(text)
+    if refseq:
+        cond = code in ["AP", "NP", "YP", "XP", "WP"]
+    else:
+        num1, num2 = len(code), len(digits)
+        cond = (num1 == 3 and num2 == 5) or (num1 == 3 and num2 == 7)
+    return cond
 
 
 def efetch(ids, db, rettype='gbwithparts', retmode='text'):
@@ -97,17 +87,20 @@ def run(db="nuccore", format_="gbwithparts", alias='', *acc):
         lines = utils.read_lines(sys.stdin, sep=None)
         ids.extend(lines)
 
-    def make_acc(text):
-        return Accession(text)
+    # Detects nucleotides
+    nucs = list(map(is_ncbi_nucleotide, ids))
 
-    # Proofread the accession numbers
-    accs = map(make_acc, ids)
-    for acc in accs:
-        if not acc.valid:
-            utils.error(f"invalid accession {acc.value}")
+    # Detects proteins
+    prots =list(map(is_ncbi_protein, ids))
 
-        if acc.is_protein():
-            db = 'protein'
+    # Swap to protins
+    if any(prots):
+        db = 'protein'
+        if not all (prots):
+            utils.error(f"invalid protein id in list {ids}")
+    else:
+        if not all(nucs):
+            utils.error(f"invalid nucleotide id in list {ids}")
 
     ids = ",".join(ids)
 
