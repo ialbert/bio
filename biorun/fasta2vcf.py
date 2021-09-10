1e4ace59dfc6508c8f48ec83a3e1f8c367241c7a
Istvan Albert <istvan.albert@gmail.com>
2021-09-09 23:01:28 -0400

    docs updated by ialbert

--- biorun/fasta2vcf.py
+++ biorun/fasta2vcf.py
@@ -8,26 +8,28 @@ from biorun import align
 def run(*fnames):
 
     #fname = '../test/data/mafft.fa'
-    stream = utils.open_streams(fnames=fnames)
+    streams = utils.open_streams(fnames=fnames)
 
-    stream = next(stream)
+    for stream in streams:
 
-    recs = utils.fasta_parser(stream)
+        try:
+            recs = utils.fasta_parser(stream)
+            ref = next(recs)
+            query = next(recs)
+        except StopIteration as exc:
+            continue
 
-    ref = next(recs)
-    query = next(recs)
+        vcfdict = align.find_variants(ref, query)
 
-    vcfdict = align.find_variants(ref, query)
+        print('##fileformat=VCFv4.2')
+        print('##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">')
+        print('##FILTER=<ID=PASS,Description="All filters passed">')
+        print('##INFO=<ID=TYPE,Number=1,Type=String,Description="Type of the variant">')
+        print(f'##contig=<ID={ref.name},length={len(ref.seq.strip("-"))},assembly={ref.name}>')
+        print(f"#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t{query.name}")
 
-    print('##fileformat=VCFv4.2')
-    print('##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">')
-    print('##FILTER=<ID=PASS,Description="All filters passed">')
-    print('##INFO=<ID=TYPE,Number=1,Type=String,Description="Type of the variant">')
-    print(f'##contig=<ID={ref.name},length={len(ref.seq.strip("-"))},assembly={ref.name}>')
-    print(f"#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t{query.name}")
-
-    for value in vcfdict.values():
-        print("\t".join(value))
+        for value in vcfdict.values():
+            print("\t".join(value))
 
 if __name__ == '__main__':
     plac.call(run)
