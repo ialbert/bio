f4c55b8594b51a4108e46232a4cdff0735212aeb
Istvan Albert <istvan.albert@gmail.com>
2021-09-23 13:27:46 -0400

    escape files

--- biorun/test.py
+++ biorun/test.py
@@ -1,25 +1,32 @@
-import os, sys
-from biorun.libs import placlib as plac
-import subprocess, difflib
+import difflib
+import os
+import subprocess
+import sys
 from subprocess import PIPE
+
 from tqdm import tqdm
 
-join = os.path.join
+from biorun.libs import placlib as plac
+
+
+def join(*args):
+    return os.path.abspath(os.path.join(*args))
 
 # The path to the current file.
-CURR_DIR = os.path.abspath(os.path.dirname(__file__))
+FILE_DIR = join(os.path.dirname(__file__))
 
 # The default data directory.
-DATA_DIR = os.path.join(CURR_DIR, "data")
+DATA_DIR = join(FILE_DIR, "data")
 
 RUN_DIR_NAME = "testrun"
 
 # The run directory
-RUN_DIR = os.path.expanduser(f"~/.bio/")
+RUN_DIR = join(os.path.expanduser("~"), ".bio")
 
-os.makedirs(RUN_DIR, exist_ok = True)
+os.makedirs(RUN_DIR, exist_ok=True)
 
 USAGE = join(DATA_DIR, "usage.sh")
+
 
 def parse_commands(text, flag=False):
     lines = text.splitlines()
@@ -36,7 +43,7 @@ def shell(cmd):
     proc = subprocess.run(cmd, shell=True, stdout=PIPE, stderr=PIPE)
 
     if proc.returncode != 0:
-        print (proc.stdout.decode("UTF-8"))
+        print(proc.stdout.decode("UTF-8"))
         print(proc.stderr.decode("UTF-8"))
         print("-" * 10)
         print(cmd)
@@ -46,18 +53,18 @@ def shell(cmd):
 
 
 INIT = f"""    
-    ln -s {CURR_DIR}/data/alias.txt
-    ln -s {CURR_DIR}/data/align_input.fa
-    ln -s {CURR_DIR}/data/mafft.fa
-    ln -s {CURR_DIR}/data/file1.txt
-    ln -s {CURR_DIR}/data/file2.txt
+    ln -s '{FILE_DIR}/data/alias.txt'
+    ln -s '{FILE_DIR}/data/align_input.fa'
+    ln -s '{FILE_DIR}/data/mafft.fa'
+    ln -s '{FILE_DIR}/data/file1.txt'
+    ln -s '{FILE_DIR}/data/file2.txt'
 """
 
 
 def test_setup():
     os.chdir(RUN_DIR)
-    shell(f"rm -rf {RUN_DIR_NAME}")
-    shell(f"mkdir {RUN_DIR_NAME}")
+    shell(f"rm -rf '{RUN_DIR_NAME}'")
+    shell(f"mkdir '{RUN_DIR_NAME}'")
     os.chdir(RUN_DIR_NAME)
 
     init = parse_commands(INIT)
@@ -75,14 +82,14 @@ def print_diff(expect, result):
     for diff in diffs:
         print(diff)
 
-def main():
 
+def main():
     test_setup()
 
     text = open(USAGE).read()
     cmds = parse_commands(text, flag=True)
 
-    #cmds = cmds[:5]
+    # cmds = cmds[:5]
 
     print(f"# Running {len(cmds)} tests:")
 
@@ -102,13 +109,15 @@ def main():
         if expect != result:
             print(f"running: {cmd}")
             print_diff(expect=expect, result=result)
-            print (f"\n\n(cd test/data && {cmd})\n")
+            print(f"\n\n(cd test/data && {cmd})\n")
             sys.exit(1)
 
-    print (f"# All tests completed")
+    print(f"# All tests completed")
+
 
 def run():
     plac.call(main)
+
 
 if __name__ == '__main__':
     run()
