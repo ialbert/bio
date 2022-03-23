3b270ab4cdacb5f05ae647ad8a90859a3fa079a8
Istvan Albert <istvan.albert@gmail.com>
2022-03-23 11:13:35 -0400

    commit by ialbert

--- biorun/vcf2fasta.py
+++ biorun/vcf2fasta.py
@@ -15,58 +15,125 @@ from itertools import *
 
 LIMIT = None
 
-def parse_json(fname):
+def get(adict, key):
+    return adi
+
+def parse_json(fname, patt="collection_date:sample_alias", key_name="run_accession"):
+
+    # Individual patterns to match
+    patts = patt.split(":")
+
+    # Load the json file.
     if fname:
         data = json.load(open(fname))
     else:
         data = []
 
+    # Remapping dictionary.
     remap = {}
-    for elem in data:
-        key = elem.get('run_accession') or 'xyz'
-        alias = elem.get('sample_alias') or 'sample'
-        date = elem.get('collection_date') or 'date'
-        value = f'{date}-{alias}-{key}'
+    counter = count(1)
+    for idx, elem in zip(counter, data):
+
+        # Insert the index into the dictionary.
+        elem['idx'] = str(idx)
+
+        # This field must be present to rename.
+        try:
+            key = elem[key_name]
+        except KeyError as exc:
+            sys.stderr.write(f"# Metadata error in: {fname} \n")
+            sys.stderr.write(f"# Key not found: {key_name}\n")
+            sys.exit(1)
+
+        # Build the rename pattern
+        fields = []
+        for p in patts:
+            fields.append(elem.get(p, p))
+
+        # The rename value.
+        value = "_".join(fields)
+
+        # Last pattern is the key.
         remap[key] = value
 
     return remap
 
+
 @plac.pos('fname', "input vcf file", type=Path)
 @plac.opt('json_', "json file to rename the sequences")
-def run(fname, json_=''):
+@plac.opt('chrom', "chromomosome to process (default: first sequence)")
+@plac.opt('limit', "how many variants to process (0=no limit)", type=int)
+@plac.opt('key', "renaming key (advanced use only)",)
+@plac.opt('patt', "renaming pattern (advanced use only)",)
+def run(fname, json_="", chrom="", limit=0, key="", patt=""):
     """
     Creates a FASTA representation from a multisample VCF file where each sample corresponds to a FASTA
     record containing the first ALT if present otherwise the REF.
     """
+
+    # Renaming key.
+    key_name= key or "run_accession"
+
+    # Renaming pattern
+    patt = patt or "collection_date:sample_alias"
+
+    # Parse the VCF file.
     vcf = VCF(fname, strict_gt=True)
 
-    samples = vcf.samples
+    # Select the target chromosome.
+    chrom = chrom if chrom else vcf.seqnames[0]
 
-    stream = islice(vcf, None)
+    # Chromosome filtering function.
+    def chrom_filter(v):
+        return v.CHROM == chrom
 
-    #Load the metdata
-    remap = parse_json(json_)
+    # Load the metdata
+    remap = parse_json(json_, patt=patt, key_name=key_name)
+
+    # Iterate over the VCF records.
+
+    # Stores the reference sequence
+    ref = []
 
     # Stores the sequences for each sample.
     store = []
-    for samp in samples:
+
+    # Initialize the sequence list.
+    for samp in vcf.samples:
         key = remap.get(samp, samp)
         store.append((key, []))
 
+    # Set the limits.
+    limit = None if limit < 1 else limit
+
+    # Used during debugging mostly.
+    stream = islice(vcf, limit)
+
+    # Process each variant
     for v in stream:
-        # Go over each sample
+
+        # Parallel iteration of sequences and genomtypes.
         combined = zip(store, v.genotypes)
 
-        for ((key, values), gtypes) in combined:
-            # First alternate genotype
-            for idx in gtypes[:-1]:
-                if idx > 0:
-                    values.append(v.ALT[idx - 1])
-                    break
-            else:
-                # Executes if there are no variants.
-                values.append(v.REF)
+        # All valid bases at the current index
+        bases = [v.REF] + v.ALT
 
+
+        # Collect the reference sequence.
+        ref.append(v.REF)
+
+        # Select the first ALT in each genoytpe.
+        for ((key, values), gtypes) in combined:
+            idx = gtypes[0]
+            idx = 0 if idx < 0 else idx
+            base = bases[idx]
+            values.append(base)
+
+    # Print the reference sequence.
+    seq = "".join(ref)
+    print(f">reference\n{seq}")
+
+    # Output each individual sequence.
     for (key, values) in store:
         seq = "".join(values)
         print(f">{key}\n{seq}")
